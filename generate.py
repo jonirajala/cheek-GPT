7f7afade508f898ab0782174d36c0e6ec8ee04cc
Joni Rajala <joni.rajala@L-FI-Q05N.local>
2023-04-09 12:52:52 +0300

    fix generate

--- generate.py
+++ generate.py
@@ -1,15 +1,18 @@
 import torch
+from utils import decode, encode
+from model import DecoderTransformer
+from train import N_EMBEDS, N_LAYER, BLOCK_SIZE, NUM_HEADS, DROPOUT, MODEL_PATH, DATA_PATH
 
-def generate(model, model_path, prompt=None):
-    model.load_state_dict(torch.load(model_path))
+with open(DATA_PATH, "r") as f:
+    text = f.read()
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+if __name__ == "__main__":
+    model = DecoderTransformer(vocab_size, N_EMBEDS, N_LAYER, BLOCK_SIZE, NUM_HEADS, DROPOUT)
+    model.load_state_dict(torch.load(MODEL_PATH))
     print(f"This models has {sum(p.numel() for p in model.parameters()) / 1e6} million paramters")
-    if prompt:
-        if len(prompt) > 8:
-            prompt = prompt[:8]
-            print(len(prompt))
-        elif len(prompt) < 8:
-            prompt = "".join([0] * (len(prompt)-8)) + prompt
-        prompt = torch.tensor(encode(prompt), dtype=torch.long).view(1,-1)
-    else:
-        prompt = torch.zeros((1,1), dtype=torch.long)
-    print(decode(model.generate(prompt, 300)[0].tolist()))
+    
+    prompt = torch.zeros((1,1), dtype=torch.long)
+    print(decode(model.generate(prompt, 300)[0].tolist(), chars))
